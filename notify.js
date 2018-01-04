cc2df78b0434db48dda0a1013411be78973f838d
cgroov <30381292+groov1234@users.noreply.github.com>
2018-01-04 16:48:31 -0500

    multiple windows versions included for now

--- notify.js
+++ notify.js
@@ -1,5 +1,18 @@
 // https://www.npmjs.com/package/node-notifier
 const thisisbananas = (`What the hay, ${process.env.USERNAME}?`);
+
+const NotificationCenter = require('node-notifier/notifiers/notificationcenter');
+new NotificationCenter().notify(thisisbananas);
+ 
+const NotifySend = require('node-notifier/notifiers/notifysend');
+new NotifySend().notify(thisisbananas);
+ 
+const WindowsToaster = require('node-notifier/notifiers/toaster');
+new WindowsToaster().notify(thisisbananas);
+ 
+const Growl = require('node-notifier/notifiers/growl');
+new Growl().notify(thisisbananas);
+ 
 const WindowsBalloon = require('node-notifier/notifiers/balloon');
 new WindowsBalloon().notify(thisisbananas);
 
