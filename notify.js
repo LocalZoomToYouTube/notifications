237e6e1fa9754d0437ac2bec657c45fcd9c54049
cgroov <30381292+groov1234@users.noreply.github.com>
2018-01-05 12:38:56 -0500

    should be working cross-platform

--- notify.js
+++ notify.js
@@ -1,31 +1,21 @@
 // https://www.npmjs.com/package/node-notifier
-// Message Params(options)
-const options = {
+const options = require("node-notifier-fixed")
+const path = require('path');
+
+options.notify({
   title: 'I am a Big Ugly Notification',
-  message: `I am ${process.env.USERNAME}t?`,
-  sound: true,
-  wait: false,
-  time: 5000,
-  type: 'info',
-  withFallback: false, // Should check for Growl first otherwise use OS dependant option 
-}
+  message: `I am ${process.env.USERNAME}t!`,
+  icon: path.join(__dirname, 'coulson.jpg'), // Absolute path (doesn't work on balloons)
+  sound: true, // Only Notification Center or Windows Toasters
+  wait: true // Wait with callback, until user action is taken against notification
+}, function (err, response) {
+  // Response is response from notification
+});
 
-// WILL not work in Win10 version 1709
-const WindowsToaster = require('node-notifier/notifiers/toaster'); 
-new WindowsToaster(options).notify(options);
+options.on('click', function (notifierObject, options) {
+  // Triggers if `wait: true` and user clicks notification
+});
 
-// MacOS
-const NotificationCenter = require('node-notifier/notifiers/notificationcenter');
-new NotificationCenter(options).notify(options);
-
-// Linux
-const NotifySend = require('node-notifier/notifiers/notifysend');
-new NotifySend(options).notify(options);
-
-// If Growl Installed
-const Growl = require('node-notifier/notifiers/growl');
-new Growl(options).notify(options);
-
-// Use for Win10 version 1709
-const WindowsBalloon = require('node-notifier/notifiers/balloon'); 
-new WindowsBalloon(options).notify(options);
+options.on('timeout', function (notifierObject, options) {
+  // Triggers if `wait: true` and notification closes
+});
