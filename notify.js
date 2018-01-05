cd7a4203b943e8d7c543014bc6f733fe89933f14
cgroov <30381292+groov1234@users.noreply.github.com>
2018-01-04 19:46:00 -0500

    See if this creates a note on MAC

--- notify.js
+++ notify.js
@@ -1,18 +1,31 @@
 // https://www.npmjs.com/package/node-notifier
-const thisisbananas = (`What the hay, ${process.env.USERNAME}?`);
+// Message Params(options)
+const options = {
+  title: 'I am a Big Ugly Notification',
+  message: `I am ${process.env.USERNAME}t?`,
+  sound: true,
+  wait: false,
+  time: 5000,
+  type: 'info',
+  withFallback: false, // Should check for Growl first otherwise use OS dependant option 
+}
 
+// WILL not work in Win10 version 1709
+const WindowsToaster = require('node-notifier/notifiers/toaster'); 
+new WindowsToaster(options).notify(options);
+
+// MacOS
 const NotificationCenter = require('node-notifier/notifiers/notificationcenter');
-new NotificationCenter().notify(thisisbananas);
- 
-const NotifySend = require('node-notifier/notifiers/notifysend');
-new NotifySend().notify(thisisbananas);
- 
-const WindowsToaster = require('node-notifier/notifiers/toaster');
-new WindowsToaster().notify(thisisbananas);
- 
-const Growl = require('node-notifier/notifiers/growl');
-new Growl().notify(thisisbananas);
- 
-const WindowsBalloon = require('node-notifier/notifiers/balloon');
-new WindowsBalloon().notify(thisisbananas);
+new NotificationCenter(options).notify(options);
 
+// Linux
+const NotifySend = require('node-notifier/notifiers/notifysend');
+new NotifySend(options).notify(options);
+
+// If Growl Installed
+const Growl = require('node-notifier/notifiers/growl');
+new Growl(options).notify(options);
+
+// Use for Win10 version 1709
+const WindowsBalloon = require('node-notifier/notifiers/balloon'); 
+new WindowsBalloon(options).notify(options);
